fea90f91f28553f63beecf3da78dc7401af38159
Ajmi <>
2022-07-26 22:58:16 +0530

    Cnnection changes

--- PythonInAppDemo/db_connection.py
+++ PythonInAppDemo/db_connection.py
@@ -6,25 +6,29 @@ Set-ExecutionPolicy -Scope CurrentUser
 -ExecutionPolicy Unrestricted
 """
 #create a connection string
-conString = 'Driver={SQL Server};Server=DESKTOP-CJEOG7N\SQLEXPRESS;Database=employee_db1;Trusted_Connection=yes;'
+#conString = 'Driver={SQL Server};Server=DESKTOP-CJEOG7N\SQLEXPRESS;Database=employee_db1;Trusted_Connection=yes;'
+conString2 = 'Drive={SQL Server Native Client 11.0};Server=LAPTOP-R5LS4JCT\SQLEXPRESS;Database=employee_db1;Trusted_Connection=yes;'
 
 #create a connection with the connection string
-myConn = pyodbc.connect(conString)
+#myConn = pyodbc.connect(conString)
+myConn2 = pyodbc.connect(conString2)
 
 #get the cursor object
-myCursor = myConn.cursor()
+#myCursor = myConn.cursor()
+myCursor2 = myConn2.cursor()
 
 #using cursor, execute SQL commands
-myCursor.execute('SELECT * FROM EmployeeMaster')
+#myCursor.execute('SELECT * FROM EmployeeMaster')
+myCursor2.execute('SELECT * FROM Employee_det')
 
 #to get the data
-for i in myCursor:
+for i in myCursor2:
     print(i)
 
-myCursor.execute('SELECT name FROM sys.tables')
-for i in myCursor:
+myCursor2.execute('SELECT name FROM sys.tables')
+for i in myCursor2:
     print(i)
 
 #commiting the transaction (required for all write/update/delete statements)
-myConn.commit()
-myConn.close()
+myConn2.commit()
+myConn2.close()
