29439d101955acf02beac4ae357e96fd800fe3e2
ajmins <ajmi.ns@inapp.com>
2022-07-29 09:59:39 +0530

    modified

--- SQLserverFiles/railway.sql
+++ SQLserverFiles/railway.sql
@@ -5,19 +5,26 @@ CREATE TABLE stations(
 	);
 
 CREATE TABLE trains(
-	train_id INT PRIMARY KEY, 
+	train_id INT IDENTITY PRIMARY KEY, 
 	train_name VARCHAR(20),
 	start_name VARCHAR(20),
 	dest_name VARCHAR(20),
 	stop_id INT FOREIGN KEY REFERENCES stations(station_id),
-	berth_no INT,
-	wait_list INT
+	berth_fill INT,
+	wait_list_fill INT
 );
 CREATE TABLE passengers(
-	passenger_id INT PRIMARY KEY,
-	passenger_name VARCHAR,
-	dest_name VARCHAR(20)
+	passenger_id INT IDENTITY PRIMARY KEY,
+	passenger_name VARCHAR(20),
+	station_id INT FOREIGN KEY REFERENCES stations(station_id),
+	train_id INT FOREIGN KEY REFERENCES trains(train_id)
 );
+CREATE TABLE waitlist(
+	passenger_id INT IDENTITY PRIMARY KEY,
+	passenger_name VARCHAR(20),
+	station_id INT FOREIGN KEY REFERENCES stations(station_id),
+	train_id INT FOREIGN KEY REFERENCES trains(train_id)
+)
 INSERT INTO stations VALUES 
 (0,'TVM'),
 (1,'ALP'),
@@ -25,7 +32,15 @@ INSERT INTO stations VALUES
 (3,'KZK');
 
 INSERT INTO trains VALUES
-(1,'TVM_ALP','TVM','ALP',1,5,2),
-(2,'TVM_ERN','TVM','ERN',2,5,2),
-(3,'TVM_KZK','TVM','KZK',3,5,2);
+('TVM_ALP','TVM','ALP',1,0,0),
+('TVM_ERN','TVM','ERN',2,0,0),
+('TVM_KZK','TVM','KZK',3,0,0);
 
+SELECT * FROM stations
+SELECT * FROM trains
+SELECT * FROM passengers
+SELECT * FROM waitlist
+--DROP TABLE stations
+--DROP TABLE trains
+--DROP TABLE passengers
+--DROP TABLE waitlist
